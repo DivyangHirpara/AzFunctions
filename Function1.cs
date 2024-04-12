6d106fd1c393efddccef5928c631cae55c0f1e1b
DivyangHirpara <divyanghirpara5@gmail.com>
2024-04-12 18:07:27 +0530

    route change

--- Function1.cs
+++ Function1.cs
@@ -14,7 +14,7 @@ namespace AzFunctionExample
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "callfunction")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
